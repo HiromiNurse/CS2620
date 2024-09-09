709894bb1e8b246d849e9e4b467fd05122d48ced
Hiromi Nurse <90805655+HiromiNurse@users.noreply.github.com>
2024-09-09 17:36:13 -0500

    Added files
    Added the imageFunctions.py and mainOld.py files to house new functions and old program. Changed main.py to reflect those changes.

--- imageFunctions.py
+++ imageFunctions.py
@@ -9,10 +9,11 @@ def greyscale_Corrected(image):
             red = pixel[0]
             green = pixel[1]
             blue = pixel[2]
-            greyscalec_value = int((red*.21)+(green*.71)+(blue*.08)) // 3
+            greyscalec_value = int((red * .21) + (green * .71) + (blue * .08)) // 3
             data[x, y] = (greyscalec_value, greyscalec_value, greyscalec_value)
         return image
-    
+
+
 def greyscale(image):
     data = image.load()
     for y in range(image.height):
@@ -21,9 +22,10 @@ def greyscale(image):
             red = pixel[0]
             green = pixel[1]
             blue = pixel[2]
-            greyscale_value = (red+green+blue) // 3
+            greyscale_value = (red + green + blue) // 3
             data[x, y] = (greyscale_value, greyscale_value, greyscale_value)
     return image
+
 
 def colorIsolationRed(image):
     data = image.load()
@@ -33,12 +35,13 @@ def colorIsolationRed(image):
             red = pixel[0]
             green = pixel[1]
             blue = pixel[2]
-            greyscale_value = (red+green+blue) // 3
-            if red > 65 and (red-green) > 15 and (red-blue) > 25 and red > greyscale_value:
+            greyscale_value = (red + green + blue) // 3
+            if red > 65 and (red - green) > 15 and (red - blue) > 25 and red > greyscale_value:
                 data[x, y] = (red, green, blue)
             else:
-                data[x, y] = (greyscale_value, greyscale_value, greyscale_value)    
+                data[x, y] = (greyscale_value, greyscale_value, greyscale_value)
     return image
+
 
 def colorIsolationGreen(image):
     data = image.load()
@@ -48,12 +51,13 @@ def colorIsolationGreen(image):
             red = pixel[0]
             green = pixel[1]
             blue = pixel[2]
-            greyscale_value = (red+green+blue) // 3
-            if green > 45 and (green-red) > 15 and (green-blue) > 15 and green > greyscale_value:
+            greyscale_value = (red + green + blue) // 3
+            if green > 45 and (green - red) > 15 and (green - blue) > 15 and green > greyscale_value:
                 data[x, y] = (red, green, blue)
             else:
                 data[x, y] = (greyscale_value, greyscale_value, greyscale_value)
     return image
+
 
 def colorIsolationBlue(image):
     data = image.load()
@@ -63,12 +67,13 @@ def colorIsolationBlue(image):
             red = pixel[0]
             green = pixel[1]
             blue = pixel[2]
-            greyscale_value = (red+green+blue) // 3
-            if blue > 65 and (blue-green) > 15 and (blue-red) > 25 and blue > greyscale_value:
+            greyscale_value = (red + green + blue) // 3
+            if blue > 65 and (blue - green) > 15 and (blue - red) > 25 and blue > greyscale_value:
                 data[x, y] = (red, green, blue)
             else:
                 data[x, y] = (greyscale_value, greyscale_value, greyscale_value)
     return image
+
 
 def xAxisMirrorL(image):
     data = image.load()
@@ -78,8 +83,9 @@ def xAxisMirrorL(image):
             red = pixel[0]
             green = pixel[1]
             blue = pixel[2]
-            data[image.width-x-1, y] = (red, green, blue)
+            data[image.width - x - 1, y] = (red, green, blue)
     return image
+
 
 def xAxisMirrorR(image):
     data = image.load()
@@ -89,9 +95,10 @@ def xAxisMirrorR(image):
             red = pixel[0]
             green = pixel[1]
             blue = pixel[2]
-            if x >= (image.width//2):
+            if x >= (image.width // 2):
                 data[image.width - 1 - x, y] = (red, green, blue)
     return image
+
 
 def yAxisMirrorT(image):
     data = image.load()
@@ -101,8 +108,9 @@ def yAxisMirrorT(image):
             red = pixel[0]
             green = pixel[1]
             blue = pixel[2]
-            data[x, image.height-1 - y] = (red, green, blue)
+            data[x, image.height - 1 - y] = (red, green, blue)
     return image
+
 
 def yAxisMirrorB(image):
     data = image.load()
@@ -113,15 +121,24 @@ def yAxisMirrorB(image):
             green = pixel[1]
             blue = pixel[2]
             if y >= (image.height // 2):
-                data[x, image.height-1-y] = (red, green, blue)
+                data[x, image.height - 1 - y] = (red, green, blue)
     return image
 
-def diagonalMirrorT(image):
+
+def diagonalMirrorT(image):  # fix
     data = image.load()
     for y in range(image.height):
         for x in range(image.width):
-            data[x,y] = data[y,x]
+            data[x, y] = data[x, y]
     return image
+
+
+def diagonalMirrorB(image):  # fix
+    data = image.load()
+    for y in range(image.height):
+        for x in range(image.width):
+            data[image.image.width - x - 1, y] = data[y, x]
+
 
 def colorInvert(image):
     data = image.load()
@@ -131,8 +148,9 @@ def colorInvert(image):
             red = pixel[0]
             green = pixel[1]
             blue = pixel[2]
-            data[x, y] = (255-red, 255-green, 255-blue)
+            data[x, y] = (255 - red, 255 - green, 255 - blue)
     return image
+
 
 def xAxisFlip(image):
     data = image.load()
@@ -142,8 +160,9 @@ def xAxisFlip(image):
             pixel = data[x, y]
             x_pixels.append(pixel)
         for x in range(image.width):
-            data[x, y] = x_pixels[image.width-x-1]  
+            data[x, y] = x_pixels[image.width - x - 1]
     return image
+
 
 def yAxisFlip(image):
     data = image.load()
@@ -153,13 +172,15 @@ def yAxisFlip(image):
             pixel = data[x, y]
             y_pixels.append(pixel)
         for y in range(image.height):
-            data[x, y] = y_pixels[image.height-y-1]
+            data[x, y] = y_pixels[image.height - y - 1]
     return image
+
 
 def flipDiagonal(image):
-    data = image.load()
-
+    image = yAxisFlip(image)
+    image = xAxisFlip(image)
     return image
+
 
 def rotateClockwise(image):
     data = image.load()
@@ -168,8 +189,9 @@ def rotateClockwise(image):
     for y in range(image.height):
         for x in range(image.width):
             pixel = data[x, y]
-            rotated_image_data[rotated_image.width-1-y, x] = pixel
-    return image
+            rotated_image_data[rotated_image.width - 1 - y, x] = pixel
+    return rotated_image
+
 
 def rotateCClockwise(image):
     data = image.load()
@@ -179,4 +201,4 @@ def rotateCClockwise(image):
         for x in range(image.width):
             pixel = data[x, y]
             rotated_image_data[y, image.width - 1 - x] = pixel
-    return image
+    return rotated_image
--- main.py
+++ main.py
@@ -2,292 +2,104 @@ from os import listdir
 from imageFunctions import *
 
 
-# def change_image(change_type, image, data):
-#     match change_type:
-#         case '0':
-#             for y in range(image.height):
-#                 for x in range(image.width):
-#                     pixel = data[x, y]
-#                     red = pixel[0]
-#                     green = pixel[1]
-#                     blue = pixel[2]
-#                     data[x, y] = (red, green, blue)
-#             image.save("image_out.png")
-#         case '1':
-#             for y in range(image.height):
-#                 for x in range(image.width):
-#                     pixel = data[x, y]
-#                     red = pixel[0]
-#                     green = pixel[1]
-#                     blue = pixel[2]
-#                     greyscalec_value = int((red*.21)+(green*.71)+(blue*.08)) // 3
-#                     data[x, y] = (greyscalec_value, greyscalec_value, greyscalec_value)
-#             image.save("image_out.png")
-#         case '2':
-#             for y in range(image.height):
-#                 for x in range(image.width):
-#                     pixel = data[x, y]
-#                     red = pixel[0]
-#                     green = pixel[1]
-#                     blue = pixel[2]
-#                     greyscale_value = (red+green+blue) // 3
-#                     data[x, y] = (greyscale_value, greyscale_value, greyscale_value)
-#             image.save("image_out.png")
-#         case '3':
-#             for y in range(image.height):
-#                 for x in range(image.width):
-#                     pixel = data[x, y]
-#                     red = pixel[0]
-#                     green = pixel[1]
-#                     blue = pixel[2]
-#                     greyscale_value = (red+green+blue) // 3
-#                     if red > 65 and (red-green) > 15 and (red-blue) > 25 and red > greyscale_value:
-#                         data[x, y] = (red, green, blue)
-#                     else:
-#                         data[x, y] = (greyscale_value, greyscale_value, greyscale_value)
-#             image.save("image_out.png")
-#         case '4':
-#             for y in range(image.height):
-#                 for x in range(image.width):
-#                     pixel = data[x, y]
-#                     red = pixel[0]
-#                     green = pixel[1]
-#                     blue = pixel[2]
-#                     greyscale_value = (red+green+blue) // 3
-#                     if green > 45 and (green-red) > 15 and (green-blue) > 15 and green > greyscale_value:
-#                         data[x, y] = (red, green, blue)
-#                     else:
-#                         data[x, y] = (greyscale_value, greyscale_value, greyscale_value)
-#             image.save("image_out.png")
-#         case '5':
-#             for y in range(image.height):
-#                 for x in range(image.width):
-#                     pixel = data[x, y]
-#                     red = pixel[0]
-#                     green = pixel[1]
-#                     blue = pixel[2]
-#                     greyscale_value = (red+green+blue) // 3
-#                     if blue > 65 and (blue-green) > 15 and (blue-red) > 25 and blue > greyscale_value:
-#                         data[x, y] = (red, green, blue)
-#                     else:
-#                         data[x, y] = (greyscale_value, greyscale_value, greyscale_value)
-#             image.save("image_out.png")
-#         case '6':
-#             for y in range(image.height):
-#                 for x in range(image.width):
-#                     pixel = data[x, y]
-#                     red = pixel[0]
-#                     green = pixel[1]
-#                     blue = pixel[2]
-#                     data[x, y] = (255-red, 255-green, 255-blue)
-#             image.save("image_out.png")
-#         case '7':
-#             for y in range(image.height):
-#                 for x in range(image.width):
-#                     data[x, y] = (5, 6, 7)
-#             image.save("image_out.png")
-#         case '8':
-#             for y in range(image.height):
-#                 for x in range(image.width):
-#                     pixel = data[x, y]
-#                     red = pixel[0]
-#                     green = pixel[1]
-#                     blue = pixel[2]
-#                     data[image.width-x-1, y] = (red, green, blue)
-#             image.save("image_out.png")
-#         case '9':
-#             for y in range(image.height):
-#                 for x in range(image.width):
-#                     pixel = data[x, y]
-#                     red = pixel[0]
-#                     green = pixel[1]
-#                     blue = pixel[2]
-#                     if x >= (image.width//2):
-#                         data[image.width - 1 - x, y] = (red, green, blue)
-#             image.save("image_out.png")
-#         case '10':
-#             for y in range(image.height):
-#                 for x in range(image.width):
-#                     pixel = data[x, y]
-#                     red = pixel[0]
-#                     green = pixel[1]
-#                     blue = pixel[2]
-#                     data[x, image.height-1 - y] = (red, green, blue)
-#             image.save("image_out.png")
-#         case '11':
-#             for y in range(image.height):
-#                 for x in range(image.width):
-#                     pixel = data[x, y]
-#                     red = pixel[0]
-#                     green = pixel[1]
-#                     blue = pixel[2]
-#                     if y >= (image.height // 2):
-#                         data[x, image.height-1-y] = (red, green, blue)
-#             image.save("image_out.png")
-#         case '12':
-#             for y in range(image.height):
-#                 x_pixels = []
-#                 for x in range(image.width):
-#                     pixel = data[x, y]
-#                     x_pixels.append(pixel)
-#                 for x in range(image.width):
-#                     data[x, y] = x_pixels[image.width-x-1]
-#             image.save("image_out.png")
-#         case '13':
-#             for x in range(image.width):
-#                 y_pixels = []
-#                 for y in range(image.height):
-#                     pixel = data[x, y]
-#                     y_pixels.append(pixel)
-#                 for y in range(image.height):
-#                     data[x, y] = y_pixels[image.height-y-1]
-#             image.save("image_out.png")
-#         case '14':
-#             for y in range(image.height):
-#                 x_pixels = []
-#                 for x in range(image.width):
-#                     pixel = data[x, y]
-#                     x_pixels.append(pixel)
-#                 for x in range(image.width):
-#                     data[x, y] = x_pixels[image.width-x-1]
-#             for x in range(image.width):
-#                 y_pixels = []
-#                 for y in range(image.height):
-#                     pixel = data[x, y]
-#                     y_pixels.append(pixel)
-#                 for y in range(image.height):
-#                     data[x, y] = y_pixels[image.height-y-1]
-#             image.save("image_out.png")
-#         case '15':
-#             rotated_image = Image.new(mode="RGB", size=(image.height, image.width))
-#             rotated_image_data = rotated_image.load()
-#             for y in range(image.height):
-#                 for x in range(image.width):
-#                     pixel = data[x, y]
-#                     rotated_image_data[rotated_image.width-1-y, x] = pixel
-#             rotated_image.save("image_out.png")
-#         case '16':
-#             rotated_image = Image.new(mode="RGB", size=(image.height, image.width))
-#             rotated_image_data = rotated_image.load()
-#             for y in range(image.height):
-#                 for x in range(image.width):
-#                     pixel = data[x, y]
-#                     rotated_image_data[y, image.width - 1 - x] = pixel
-#             rotated_image.save("image_out.jpg")
-#     quit()
-
-
-# def main_menu():
-#     print("What kind of change would you like to make?")
-#     image_list = [f for f in listdir() if ".jpg" in f]
-#     image_list.append("Quit")
-#     image_number = 0
-#     for image in image_list:
-#         print(f"{image_number}: {image}")
-#         image_number += 1
-
-#     selected_image_number = int(input("Input the number corresponding to the image name: "))
-#     selected_image = image_list[selected_image_number]
-#     if image_list[selected_image_number] == "Quit":
-#         quit()
-#     image = Image.open(selected_image)
-#     data = image.load()
-
-#     print(f"Selected Image: {selected_image}")
-#     print("What kind of change would you like to make?")
-
-#     options_list = ["Normal", "Greyscale corrected", "Greyscale", "Color Isolation Red",
-#                     "Color Isolation Green", "Color Isolation Blue", "Invert", "Blind Filter",
-#                     "X-axis Mirror (Left)", "X-axis Mirror (Right)", "Y-axis Mirror (Top)",
-#                     "Y-axis Mirror (Bottom)", "Flip X-axis", "Flip Y-axis", "Flip Diagonal",
-#                     "Rotate 90(Clockwise)", "Rotate 90(Counter-Clockwise)",
-#                     "Quit/q"]
-#     column_height = len(options_list)//3
-#     extra_options = len(options_list) % 3
-#     if extra_options == 0:
-#         for i in range(0, len(options_list) // 3):
-#             print(f"{i:3}: {options_list[i]:25}{(i+column_height):3}: {options_list[i+column_height]:25}"
-#                   f"{(i+(column_height*2)):3}: {options_list[i+(column_height*2)]:25}")
-#     elif extra_options == 1:
-#         for i in range(0, len(options_list) // 3):
-#             print(f"{i:3}: {options_list[i]:25}{(i+column_height+1):3}: {options_list[i+1+column_height]:25}"
-#                   f"{(i+(column_height*2)+1):3}: {options_list[i+(column_height*2)+1]:25}")
-#         print(f"{column_height:3}: {options_list[column_height]:25}")
-#     elif extra_options == 2:
-#         for i in range(0, len(options_list) // 3):
-#             print(f"{i:3}: {options_list[i]:25}{(i+column_height+1):3}: {options_list[i+1+column_height]:25}"
-#                   f"{(i+(column_height*2)+2):3}: {options_list[i+(column_height*2)+2]:25}")
-#         print(f"{column_height:3}: {options_list[column_height]:25}"
-#               f"{(column_height*2)+1:3}: {options_list[(column_height*2)+1]:25}")
-
-#     change_type = input("Change: ").strip()
-#     print(f"Selected Change: {options_list[int(change_type)]}")
-#     change_image(change_type, image, data)
-
 def main_menu():
-    print("What kind of change would you like to make?")
-    image_list = [f for f in listdir() if ".jpg" in f]
-    image_list.append("Quit")
-    image_number = 0
-    for image in image_list:
-        print(f"{image_number}: {image}")
-        image_number += 1
-    selected_image_number = int(input("Input the number corresponding to the image name: "))
-    selected_image = image_list[selected_image_number]
-    if image_list[selected_image_number] == "Quit":
-        quit()
+    try:
+        print("What kind of change would you like to make?")
+        image_list = [f for f in listdir() if ".jpg" in f]
+        image_list.append("Quit")
+        image_number = 0
+        for image in image_list:
+            print(f"{image_number}: {image}")
+            image_number += 1
+        selected_image_number = int(input("Input the number corresponding to the image name: "))
+        selected_image = image_list[selected_image_number]
+        if image_list[selected_image_number] == "Quit":
+            return
+        image = Image.open(selected_image)
 
-    options_list = ["Normal", "Greyscale corrected", "Greyscale", "Color Isolation Red",
-                    "Color Isolation Green", "Color Isolation Blue", "Invert", "Blind Filter",
-                    "X-axis Mirror (Left)", "X-axis Mirror (Right)", "Y-axis Mirror (Top)",
-                    "Y-axis Mirror (Bottom)", "Flip X-axis", "Flip Y-axis", "Flip Diagonal",
-                    "Rotate 90(Clockwise)", "Rotate 90(Counter-Clockwise)",
-                    "Quit/q"]
-    
-    column_height = len(options_list)//3
-    extra_options = len(options_list) % 3
-    if extra_options == 0:
-        for i in range(0, len(options_list) // 3):
-            print(f"{i:3}: {options_list[i]:25}{(i+column_height):3}: {options_list[i+column_height]:25}"
-                  f"{(i+(column_height*2)):3}: {options_list[i+(column_height*2)]:25}")
-    elif extra_options == 1:
-        for i in range(0, len(options_list) // 3):
-            print(f"{i:3}: {options_list[i]:25}{(i+column_height+1):3}: {options_list[i+1+column_height]:25}"
-                  f"{(i+(column_height*2)+1):3}: {options_list[i+(column_height*2)+1]:25}")
-        print(f"{column_height:3}: {options_list[column_height]:25}")
-    elif extra_options == 2:
-        for i in range(0, len(options_list) // 3):
-            print(f"{i:3}: {options_list[i]:25}{(i+column_height+1):3}: {options_list[i+1+column_height]:25}"
-                  f"{(i+(column_height*2)+2):3}: {options_list[i+(column_height*2)+2]:25}")
-        print(f"{column_height:3}: {options_list[column_height]:25}"
-              f"{(column_height*2)+1:3}: {options_list[(column_height*2)+1]:25}")
-        
-    change_type = input("Change: ").strip()
-    print(f"Selected Change: {options_list[int(change_type)]}")
-    match change_type:
-        case '0':
-            greyscale_Corrected(image)
-        case '1':
-            greyscale(image)
-        case '2':
-            colorIsolationRed(image)
-        case '3':
+        options_list = ["Normal", "Greyscale corrected", "Greyscale", "Color Isolation Red",
+                        "Color Isolation Green", "Color Isolation Blue", "Invert", "Blind Filter",
+                        "X-axis Mirror (Left)", "X-axis Mirror (Right)", "Y-axis Mirror (Top)",
+                        "Y-axis Mirror (Bottom)", "Flip X-axis", "Flip Y-axis", "Flip Diagonal",
+                        "Rotate 90(Clockwise)", "Rotate 90(Counter-Clockwise)", "Diagonal Mirror (Top Right)",
+                        "Diagonal Mirror (Bottom Left)",
+                        "Quit/q"]
 
-        case '4':
+        column_height = len(options_list) // 3
+        extra_options = len(options_list) % 3
+        if extra_options == 0:
+            for i in range(0, len(options_list) // 3):
+                print(f"{i:3}: {options_list[i]:25}{(i + column_height):3}: {options_list[i + column_height]:25}"
+                      f"{(i + (column_height * 2)):3}: {options_list[i + (column_height * 2)]:25}")
+        elif extra_options == 1:
+            for i in range(0, len(options_list) // 3):
+                print(
+                    f"{i:3}: {options_list[i]:25}{(i + column_height + 1):3}: {options_list[i + 1 + column_height]:25}"
+                    f"{(i + (column_height * 2) + 1):3}: {options_list[i + (column_height * 2) + 1]:25}")
+            print(f"{column_height:3}: {options_list[column_height]:25}")
+        elif extra_options == 2:
+            for i in range(0, len(options_list) // 3):
+                print(
+                    f"{i:3}: {options_list[i]:25}{(i + column_height + 1):3}: {options_list[i + 1 + column_height]:25}"
+                    f"{(i + (column_height * 2) + 2):3}: {options_list[i + (column_height * 2) + 2]:25}")
+            print(f"{column_height:3}: {options_list[column_height]:25}"
+                  f"{(column_height * 2) + 1:3}: {options_list[(column_height * 2) + 1]:25}")
 
-        case '5':
+        change_type = input("Change: ").strip()
+        print(f"Selected Change: {options_list[int(change_type)]}")
+        match change_type:
+            case '0':
+                image.save("output_image.png")
+            case '1':
+                new_image = greyscale_Corrected(image)
+            case '2':
+                new_image = greyscale(image)
+            case '3':
+                new_image = colorIsolationRed(image)
+            case '4':
+                new_image = colorIsolationGreen(image)
+            case '5':
+                new_image = colorIsolationBlue(image)
+            case '6':
+                new_image = colorInvert(image)
+            case '7':
+                return  # holder
+            case '8':
+                new_image = xAxisMirrorL(image)
+            case '9':
+                new_image = xAxisMirrorR(image)
+            case '10':
+                new_image = yAxisMirrorT(image)
+            case '11':
+                new_image = yAxisMirrorB(image)
+            case '12':
+                new_image = xAxisFlip(image)
+            case '13':
+                new_image = yAxisFlip(image)
+            case '14':
+                new_image = flipDiagonal(image)
+            case '15':
+                new_image = rotateClockwise(image)
+            case '16':
+                new_image = rotateCClockwise(image)
+            case '17':
+                new_image = diagonalMirrorT(image)
+            case '18':
+                new_image = diagonalMirrorB(image)
 
-        case '6':
+            case _:
+                print("Not an option. Quitting...")
+                return
 
-        case '7':
+        if change_type != '0':
+            print("Saving...")
+            new_image.save("output_image.png")
 
-        case '8':
-
-        case '9':
-            
-        case '10':
+    except ValueError:
+        print("Invalid input. Try again.")
+        main_menu()
+        return
 
 
 if __name__ == "__main__":
-    while True:
-        main_menu()
+    main_menu()
