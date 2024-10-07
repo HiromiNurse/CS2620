85b19b28b8a1ad598bdebb321477b93352ef2d30
Hiromi Nurse <90805655+HiromiNurse@users.noreply.github.com>
2024-10-07 10:28:11 -0500

    ascii video changes
    made changes to ascii video to make it better. also added a color option of ascii video.

--- ascii_video.py
+++ ascii_video.py
@@ -14,24 +14,21 @@ def resize(image, new_width = 100):
 def get_ascii(image):
     ascii_str = ""
     pixels = image.getdata()
-    # for y in range(image.height):
-    #     output_frame + "\n"
-    #     for x in range(image.width):
-    #         output_frame +
     for pixel in pixels:
-        ascii_str += ASCII_CHARS[pixel[1] // 25]
+        ascii_str += ASCII_CHARS[pixel * (len(ASCII_CHARS)-1) // 255]
     return ascii_str
 
 
-ASCII_CHARS = ["@", "#", "$", "%", "?", "*", "+", ";", ":", ",", "."]
+ASCII_CHARS = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
 screen_number = int(input("Which monitor will be the input?(0-3): "))
 camera = dxcam.create(device_idx=0, output_idx=screen_number)
 camera.start()
+
 while True:
     frame = camera.get_latest_frame()
     image = Image.fromarray(frame)
     image = resize(image)
-    image.convert("L")
+    image = image.convert("L")
     ascii_str = get_ascii(image)
     ascii_str_len = len(ascii_str)
     width = image.width
@@ -40,7 +37,3 @@ while True:
         ascii_img += ascii_str[i: i+width] + "\n"
     system('cls')
     print(ascii_img)
-
-
-camera.stop()
-
---
+++ ascii_video_color.py
@@ -0,0 +1,56 @@
+import dxcam
+from PIL import Image
+from os import system, get_terminal_size
+
+
+def resize(image, new_width=100):
+    width, lines = get_terminal_size()
+    new_width = width
+    old_width, old_height = image.size
+    new_height = new_width * old_height // old_width
+    return image.resize((new_width, new_height))
+
+
+def brighten_color(r, g, b, factor=1.5):
+    r = min(int(r * factor), 255)
+    g = min(int(g * factor), 255)
+    b = min(int(b * factor), 255)
+    return r, g, b
+
+
+def rgb_to_ansi(r, g, b):
+    return f"\033[38;2;{r};{g};{b}m"
+
+
+def get_ascii(image, brightness_factor=1.5):
+    ascii_str = []
+    pixels = image.getdata()
+
+    for pixel in pixels:
+        r, g, b = pixel
+        r, g, b = brighten_color(r, g, b, brightness_factor)
+        ascii_char = ASCII_CHARS[(len(ASCII_CHARS)-1) - (sum(pixel) // 3 * (len(ASCII_CHARS) - 1) // 255)]
+        color_code = rgb_to_ansi(r, g, b)
+        ascii_str.append(f"{color_code}{ascii_char}\033[0m")
+    return ascii_str
+
+
+ASCII_CHARS = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+screen_number = int(input("Which monitor will be the input? (0-3): "))
+camera = dxcam.create(device_idx=0, output_idx=screen_number)
+camera.start()
+
+while True:
+    frame = camera.get_latest_frame()
+    image = Image.fromarray(frame)
+    image = resize(image)
+
+    ascii_str = get_ascii(image, brightness_factor=1.5)
+    width = image.width
+    ascii_img = ""
+
+    for i in range(0, len(ascii_str), width):
+        ascii_img += ''.join(ascii_str[i: i + width]) + "\n"
+
+    system('cls')
+    print(ascii_img)
